686682db5c4b4a28907aaa3e3f0258d1f91b6271
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-11-15 16:17:37 -0300

    feat: css styles
    flexbox center alignings items

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import "./assets/styles/custom.scss";
 import "./App.css";
-import Navbar from "components/Navbar";
+import Home from "pages/Home";
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-      <h1 className="">Início do Projeto</h1>;
+    <Home/>
     </>
   );
 };
